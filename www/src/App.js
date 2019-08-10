d7ccd68be7343ad1677e7d79c5b440cc3c824078
Andreas Lindhé <andreas@lindhe.io>
2019-08-10 20:57:39 +0200

    Remove contact form, portfoli and testimonials

--- www/src/App.js
+++ www/src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import About from './about/about.js'
-import Contact from './contact/contact.js'
 import Footer from './footer/footer.js'
 import Header from './header/header.js'
-import Portfolio from './portfolio/portfolio.js'
 import Resume from './resume/resume.js'
-import Testimonials from './testimonials/testimonials.js'
 import Logo from './logo.svg';
 import './App.css';
 
@@ -14,10 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <About />
-      <Contact />
       <Resume />
-      <Portfolio />
-      <Testimonials />
       <Footer />
     </div>
   );
