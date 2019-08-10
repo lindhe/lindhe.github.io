bcde3b12c212dcebd13e9107330c472ea03a20ce
Andreas Lindhé <andreas@lindhe.io>
2019-08-10 20:34:16 +0200

    Adapt App.js

--- www/src/App.js
+++ www/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import About from './about/about.js'
+import Contact from './contact/contact.js'
+import Footer from './footer/footer.js'
+import Header from './header/header.js'
+import Portfolio from './portfolio/portfolio.js'
+import Resume from './resume/resume.js'
+import Testimonials from './testimonials/testimonials.js'
+import Logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <About />
+      <Contact />
+      <Resume />
+      <Portfolio />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
